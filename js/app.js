c7aa0ddb7e9f5d59cbe5acb3c388f5ed9ca0943a
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-03-11 01:49:38 +0600

    user and users comment solve

--- js/app.js
+++ js/app.js
@@ -27,7 +27,6 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    console.log(text);
     return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
@@ -52,6 +51,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  console.log(post);
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -121,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post?.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post?.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
